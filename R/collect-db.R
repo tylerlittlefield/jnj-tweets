77f80f2f66d683511bc386b07e26c3faae71a9b2
Tyler Littlefield <tylerlittlefield@hey.com>
2020-08-31 03:38:27 +0000

    deprecate s3 usage, and start appending new tweets to database

---
+++ R/collect-db.R
@@ -0,0 +1,60 @@
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(dbplyr)
+  library(DBI)
+  library(aws.s3)
+  library(glue)
+  library(rtweet)
+})
+
+# database configuration
+dw <- config::get("datawarehouse")
+
+# connection
+con <- DBI::dbConnect(
+  odbc::odbc(), 
+  Driver   = dw$driver, 
+  Server   = dw$server, 
+  Database = dw$database, 
+  UID      = dw$uid, 
+  PWD      = dw$pwd, 
+  Port     = dw$port
+)
+
+# currently collected tweets (status IDs)
+cur_ids <- tbl(con, "jnj_twitter") %>% 
+  distinct(status_id) %>% 
+  collect()
+
+# collect new tweets
+df_new_tweets <- search_tweets2(
+  q = c('"johnson and johnson"', '"johnson & johnson"', "#jnj"),
+  n = 18000,
+  include_rts = FALSE, 
+  retryonratelimit = TRUE, 
+  verbose = FALSE, 
+  token = readRDS(".rtweet_token2.rds")
+)
+
+# get status IDs that we currently do not have
+df_append_to_db <- df_new_tweets %>% 
+  filter(!status_id %in% cur_ids$status_id) %>% 
+  flatten()
+
+# message for log
+cat(glue("[{Sys.Date()}] Collecting {nrow(df_append_to_db)} new tweets...\n\n"))
+
+# append to the database table
+dbWriteTable(con, "jnj_twitter", df_append_to_db, append = TRUE)
+
+# disconnect
+dbDisonnect(con)
+
+# cronR::cron_add(
+#   command = "cd /home/tyler/dev/jnj-tweets && /usr/bin/Rscript 'R/collect.R' >> 'R/collect.log' 2>&1",
+#   frequency = "daily",
+#   at = "10PM",
+#   id = "JNJ Tweets",
+#   tags = "#rstats, #twitter, #data",
+#   description = "Collects twitter data with 'johnson and johnson' or 'johnson & johnson' in text, everyday at 10PM."
+# )
